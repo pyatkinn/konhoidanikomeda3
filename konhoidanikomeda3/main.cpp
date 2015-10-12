9dce642ae37f6a5d7ff1ec9829319fc5ffca41ec
pyatkinn <pyatkinn@yahoo.com>
2015-10-12 11:14:57 +0300

    Initial Commit

---
+++ konhoidanikomeda3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  konhoidanikomeda3
+//
+//  Created by Никита Пяткин on 12.10.15.
+//  Copyright © 2015 Никита Пяткин. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
