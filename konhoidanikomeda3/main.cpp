ff6a38febe8e53e2425a69169e28a0045c035aa2
pyatkinn <pyatkinn@yahoo.com>
2015-10-16 22:47:59 +0300

    sfe

--- konhoidanikomeda3/main.cpp
+++ konhoidanikomeda3/main.cpp
@@ -11,6 +11,7 @@
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
-    std::cout<<"Борис";
+    std::cout<<"Борис\n";
+    std::cout<<"sfe";
     return 0;
 }
