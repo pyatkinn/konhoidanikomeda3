d9c8a47030e778b98baf03a2b65c6d19d1b7d2ff
pyatkinn <pyatkinn@yahoo.com>
2015-10-17 00:33:33 +0300

    Определение состояния класса

--- konhoidanikomeda3/main.cpp
+++ konhoidanikomeda3/main.cpp
@@ -7,11 +7,113 @@
 //
 
 #include <iostream>
+#include <math.h>
+#include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    std::cout<<"Борис\n";
-    std::cout<<"sfe";
-    return 0;
+using std::cin;
+using std::cout;
+using std::endl;
+
+class konhoida{
+private:
+    double a,l;
+public:
+    konhoida(double a=1,double l=1);
+    ~konhoida();
+    double get() const;
+    void set(double a,double l);
+    double yValue(double x);
+    double distination(double angle);
+    double rKriv1();
+    double rKriv2();
+    double rKriv3();
+    double sPetl();
+    double tPereg();
+};
+
+
+konhoida::konhoida(double a,double l)
+{
+    this->a=a;
+    this->l=l;
 }
+
+konhoida::~konhoida()
+{
+}
+
+double konhoida:: get() const
+{
+    return a;
+}
+
+void konhoida::set(double a,double l)
+{
+    this->a=a;
+    this->l=l;
+}
+
+double konhoida::yValue(double x)
+{
+    return (sqrt((l*l*x*x)/(x-a)*(x-a)-x*x));
+}
+
+double konhoida::distination(double angle)
+{
+    return (a/cos(angle)+l);
+}
+
+double konhoida::rKriv1()
+{
+    return (((l+a)*(l+a))/2);
+}
+
+double konhoida::rKriv2()
+{
+    return (((l-a)*(l-a))/2);
+}
+
+double konhoida::rKriv3()
+{
+    return ((l*sqrt(l*l-a*a))/(2*a));
+}
+
+double konhoida::sPetl()
+{
+    return (a*sqrt(l*l-a*a)-2*a*l*log((1+sqrt(l*l-a*a))/a)+l*l*acos(a/l));
+}
+
+double konhoida::tPereg()
+{
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
